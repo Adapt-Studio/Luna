837c7e1c2f0e382078e97c3550c94fb3773b1c59
Mac <machooper9@gmail.com>
2020-12-29 15:04:10 +0000

    mobile: contact done

--- components/form.js
+++ components/form.js
@@ -1,6 +1,6 @@
 export default function Form() {
 	return (
-	<form className="w-96 flex flex-col justify-evenly items-center space-y-6  mx-auto">
+	<form className="w-5/6 md:w-96 flex flex-col justify-evenly items-center space-y-6  mx-auto">
 		<input className="bg-blue-100 w-full rounded-md px-5 py-2" type="text" name="name" placeholder="Your Name"/>
 		<input className="bg-blue-100 rounded-md w-full px-5 py-2 " type="email" name="email" placeholder="Your E-Mail"/>
 		<textarea className="h-40 bg-blue-100 rounded-md w-full px-5 py-2" name="message" placeholder="Your Message"></textarea>
