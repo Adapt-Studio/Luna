cce8db59069b9722e0084a9c074321a3e324988d
Mac <machooper9@gmail.com>
2020-12-29 14:54:16 +0000

    mobile: about done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0ea158d0efe04032d98c";
+/******/ 	var hotCurrentHash = "465373f3b4beb66cdd3e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,10 @@ import Layout from '../components/layout'
 export default function About() {
   return (	
 	<Layout>
-		<div className="flex justify-evenly items-center w-4/5 mx-auto">
+		<div className="flex flex-col md:flex-row justify-evenly items-center md:w-4/5 w-5/6 mx-auto">
 	  		<img className="w-8/12" src="/images/about.png" alt="About Image"/>
-	  		<div className="max-width-4/6 flex flex-col justify-evenly items-start ml-12">
-	  			<h2 className="font-serif text-3xl">About The Luna Collection...</h2>
+	  		<div className="md:max-width-4/6 flex flex-col justify-evenly items-start md:ml-12">
+	  			<h2 className="font-serif text-2xl md:text-3xl">About The Luna Collection...</h2>
 	  			<p className="my-10">Amet eos sapiente nemo consectetur expedita quod! Aspernatur distinctio nesciunt fugiat soluta qui Fugiat deserunt exercitationem iusto maxime commodi Libero fuga quas totam non in eos excepturi? Suscipit libero assumenda.</p>
 	  			<p className="mb-8">Amet eos sapiente nemo consectetur expedita quod! Aspernatur distinctio nesciunt fugiat soluta qui Fugiat deserunt exercitationem iusto maxime commodi Libero fuga quas totam non in eos excepturi? Suscipit libero assumenda.</p>
 	  			<p>Tina x</p>
